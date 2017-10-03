600ec481cabf160763578dab28197e097f02898b
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-10-03 13:19:45 -0700

    tested and validated that endpoint works

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
-router.post('/'), function(req, res) {
+router.post('/', function(req, res) {
   const name = req.body.name;
-  const role = req.body.friend_list_id;
-  db.users.create({ name: name })
+  models.User.create({ name: name })
   .then(newOwner => { res.json(newOwner); })
-  });
 });
 
 module.exports = router;
