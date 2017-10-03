140e87b335532858d27a904e4d6d019de4af03fb
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-10-03 11:59:51 -0700

    belongsTo User! not hasMany

--- models/friendslist.js
+++ models/friendslist.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        FriendsList.hasMany(models.Users);
+        FriendsList.belongsTo(models.Users);
       }
     }
   });
