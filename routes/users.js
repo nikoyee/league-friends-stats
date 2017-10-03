42aa812a03a991ce51ec28b36c75578d9744476b
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-10-03 13:03:17 -0700

    added a post router for creating a user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
+});
+
+router.post('/'), function(req, res) {
+  const name = req.body.name;
+  const role = req.body.friend_list_id;
+  db.users.create({ name: name })
+  .then(newOwner => { res.json(newOwner); })
+  });
 });
 
 module.exports = router;
